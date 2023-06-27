3030d31133203e79e60cbb5992aa804afe53069f
mphoChuene <mphochuene42@gmail.com>
2023-06-27 15:35:44 +0200

    completed button

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,9 +1,94 @@
 import React from "react";
-import "./Rooms.css";
+import styles from "./Rooms.module.css";
+
+import {
+  basic,
+  comfort,
+  blueroom,
+  royal,
+  goldroom,
+  deluxroom,
+  superdelux,
+} from "../imports";
+
 const Rooms = () => {
   return (
-    <div className="container">
-      <h1 className="heading">Available rooms</h1>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Available rooms</h1>
+
+      <div className={styles.room_container}>
+        <div className={styles.room_subcontainer}>
+          <div className={styles.slider_container}>
+            <div className={styles.box}>
+              <img src={basic} />
+              <h4>Basic room</h4>
+              <h5>2 bathrooms</h5>
+              <h2 className={styles.prices}>
+                R500 / <span className={styles.sub_price}>night </span>
+              </h2>
+              <button>Reserve</button>
+            </div>
+          </div>
+
+          <div className={styles.slider_container}>
+            <div className={styles.box}>
+              <img src={basic} />
+              <h4>Basic room</h4>
+              <h5>2 bathrooms</h5>
+              <h2 className={styles.prices}>
+                R500 / <span className={styles.sub_price}>night </span>
+              </h2>
+              <button>Reserve</button>
+            </div>
+          </div>
+          <div className={styles.slider_container}>
+            <div className={styles.box}>
+              <img src={basic} />
+              <h4>Basic room</h4>
+              <h5>2 bathrooms</h5>
+              <h2 className={styles.prices}>
+                R500 / <span className={styles.sub_price}>night </span>
+              </h2>
+              <button>Reserve</button>
+            </div>
+          </div>
+
+          <div className={styles.slider_container}>
+            <div className={styles.box}>
+              <img src={basic} />
+              <h4>Basic room</h4>
+              <h5>2 bathrooms</h5>
+              <h2 className={styles.prices}>
+                R500 / <span className={styles.sub_price}>night </span>
+              </h2>
+              <button>Reserve</button>
+            </div>
+          </div>
+          <div className={styles.slider_container}>
+            <div className={styles.box}>
+              <img src={basic} />
+              <h4>Basic room</h4>
+              <h5>2 bathrooms</h5>
+              <h2 className={styles.prices}>
+                R500 / <span className={styles.sub_price}>night </span>
+              </h2>
+              <button>Reserve</button>
+            </div>
+          </div>
+
+          <div className={styles.slider_container}>
+            <div className={styles.box}>
+              <img src={basic} />
+              <h4>Basic room</h4>
+              <h5>2 bathrooms</h5>
+              <h2 className={styles.prices}>
+                R500 / <span className={styles.sub_price}>night </span>
+              </h2>
+              <button>Reserve</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

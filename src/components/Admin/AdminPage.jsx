be8fa10ce5884d3f73c156a624b02885084e7d9c
mphoChuene <mphochuene42@gmail.com>
2023-06-29 14:37:58 +0200

    admin page creation

---
+++ src/components/Admin/AdminPage.jsx
@@ -0,0 +1,191 @@
+import React from "react";
+import "./AdminPage.css";
+
+export const AdminPage = () => {
+  return (
+    <div className="admin-page">
+      <div className="div">
+        <div className="overlap">
+          <div className="text-wrapper">Bookings</div>
+        </div>
+        <div className="overlap-group">
+          <div className="text-wrapper-2">All rooms</div>
+        </div>
+        <div className="text-wrapper-3">Whiteman Lodge</div>
+        <div className="overlap-2">
+          <div className="overlap-3">
+            <div className="rectangle" />
+            <h1 className="h-1">Hotel Suits</h1>
+            <div className="component">
+              <img className="img" alt="Rectangle" src="rectangle-23-9.svg" />
+              <div className="frame">
+                <div className="text-wrapper-4">Mr Simon</div>
+                <img className="vector" alt="Vector" src="vector-18.svg" />
+                <div className="text-wrapper-5">x1</div>
+                <img className="vector-2" alt="Vector" src="vector-19.svg" />
+                <div className="text-wrapper-6">02 July- 03 July</div>
+              </div>
+            </div>
+          </div>
+          <img className="rectangle-2" alt="Rectangle" src="rectangle-15.png" />
+          <div className="component-2">
+            <img
+              className="rectangle-3"
+              alt="Rectangle"
+              src="rectangle-23-7.svg"
+            />
+            <div className="frame">
+              <div className="text-wrapper-4">Mr Lenon</div>
+              <img className="vector" alt="Vector" src="vector-14.svg" />
+              <div className="text-wrapper-5">x1</div>
+              <img className="vector-2" alt="Vector" src="vector-15.svg" />
+              <div className="text-wrapper-6">02 June- 03 June</div>
+            </div>
+          </div>
+        </div>
+        <img className="rectangle-4" alt="Rectangle" src="rectangle-16.png" />
+        <img className="rectangle-5" alt="Rectangle" src="rectangle-17.png" />
+        <img className="rectangle-6" alt="Rectangle" src="rectangle-18.png" />
+        <img className="rectangle-7" alt="Rectangle" src="rectangle-19.png" />
+        <img className="rectangle-8" alt="Rectangle" src="rectangle-20.png" />
+        <img className="rectangle-9" alt="Rectangle" src="rectangle-21.png" />
+        <img className="rectangle-10" alt="Rectangle" src="rectangle-22.png" />
+        <div className="overlap-4">
+          <div className="text-wrapper-7">Booking dates:</div>
+          <div className="text-wrapper-7">Booking dates:</div>
+        </div>
+        <div className="overlap-group-2">
+          <div className="text-wrapper-7">Booking dates:</div>
+          <div className="text-wrapper-7">Booking dates:</div>
+        </div>
+        <div className="text-wrapper-8">Booking dates:</div>
+        <div className="text-wrapper-9">Booking dates:</div>
+        <div className="text-wrapper-10">Booking dates:</div>
+        <div className="text-wrapper-11">Booking dates:</div>
+        <div className="text-wrapper-12">Booking dates:</div>
+        <p className="name-mr-john-letago">Name:&nbsp;&nbsp;Mr John Letago</p>
+        <div className="component-3">
+          <img
+            className="rectangle-11"
+            alt="Rectangle"
+            src="rectangle-23-8.svg"
+          />
+          <div className="frame">
+            <div className="text-wrapper-4">Mr Mpho</div>
+            <img className="vector" alt="Vector" src="vector-16.svg" />
+            <div className="text-wrapper-5">x1</div>
+            <img className="vector-2" alt="Vector" src="vector-17.svg" />
+            <div className="text-wrapper-6">12 June- 16 June</div>
+          </div>
+        </div>
+        <div className="component-4">
+          <img
+            className="rectangle-12"
+            alt="Rectangle"
+            src="rectangle-23-10.svg"
+          />
+          <div className="frame">
+            <div className="text-wrapper-4">Mr Harvey</div>
+            <img className="vector" alt="Vector" src="vector-20.svg" />
+            <div className="text-wrapper-5">x1</div>
+            <img className="vector-2" alt="Vector" src="vector-21.svg" />
+            <div className="text-wrapper-6">12 June- 13 June</div>
+          </div>
+        </div>
+        <div className="component-5">
+          <img
+            className="rectangle-13"
+            alt="Rectangle"
+            src="rectangle-23-6.svg"
+          />
+          <div className="frame">
+            <div className="text-wrapper-4">Mr Lethabo</div>
+            <img className="vector" alt="Vector" src="vector-12.svg" />
+            <div className="text-wrapper-5">x1</div>
+            <img className="vector-2" alt="Vector" src="vector-13.svg" />
+            <div className="text-wrapper-6">12 June- 16 June</div>
+          </div>
+        </div>
+        <div className="component-6">
+          <img
+            className="rectangle-14"
+            alt="Rectangle"
+            src="rectangle-23.svg"
+          />
+          <div className="frame">
+            <div className="text-wrapper-4">Mrs Masana</div>
+            <img className="vector" alt="Vector" src="vector-2.svg" />
+            <div className="text-wrapper-5">x1</div>
+            <img className="vector-2" alt="Vector" src="vector-3.svg" />
+            <div className="text-wrapper-6">25 June-26 June</div>
+          </div>
+        </div>
+        <div className="component-7">
+          <img
+            className="rectangle-15"
+            alt="Rectangle"
+            src="rectangle-23-2.svg"
+          />
+          <div className="frame">
+            <div className="text-wrapper-4">Mr Chuene</div>
+            <img className="vector" alt="Vector" src="vector-4.svg" />
+            <div className="text-wrapper-5">x1</div>
+            <img className="vector-2" alt="Vector" src="vector-5.svg" />
+            <div className="text-wrapper-6">28 July- 6 Aug</div>
+          </div>
+        </div>
+        <div className="component-8">
+          <img
+            className="rectangle-16"
+            alt="Rectangle"
+            src="rectangle-23-3.svg"
+          />
+          <div className="frame">
+            <div className="text-wrapper-4">Mrs Faith</div>
+            <img className="vector" alt="Vector" src="vector-6.svg" />
+            <div className="text-wrapper-5">x1</div>
+            <img className="vector-2" alt="Vector" src="vector-7.svg" />
+            <div className="text-wrapper-6">20 Sept- 26 Sept</div>
+          </div>
+        </div>
+        <div className="component-9">
+          <img
+            className="rectangle-17"
+            alt="Rectangle"
+            src="rectangle-23-4.svg"
+          />
+          <div className="frame">
+            <div className="text-wrapper-4">Mr Edwin</div>
+            <img className="vector" alt="Vector" src="vector-8.svg" />
+            <div className="text-wrapper-5">x1</div>
+            <img className="vector-2" alt="Vector" src="vector-9.svg" />
+            <div className="text-wrapper-6">05 June- 10 June</div>
+          </div>
+        </div>
+        <div className="component-10">
+          <img
+            className="rectangle-18"
+            alt="Rectangle"
+            src="rectangle-23-5.svg"
+          />
+          <div className="frame">
+            <div className="text-wrapper-4">Mr Mpetle</div>
+            <img className="vector" alt="Vector" src="vector-10.svg" />
+            <div className="text-wrapper-5">x1</div>
+            <img className="vector-2" alt="Vector" src="vector-11.svg" />
+            <div className="text-wrapper-6">12 July- 14 July</div>
+          </div>
+        </div>
+        <div className="frame-2">
+          <div className="text-wrapper-4">Mr Chuene</div>
+          <img className="vector" alt="Vector" src="vector.svg" />
+          <div className="text-wrapper-5">x1</div>
+          <img className="vector-2" alt="Vector" src="image.svg" />
+          <div className="text-wrapper-6">01 Aug- 16 Aug</div>
+        </div>
+        <div className="text-wrapper-13">Admin</div>
+        <img className="icon-user" alt="Icon user" src="icon-user.png" />
+      </div>
+    </div>
+  );
+};

6de28d37826d8c8b696b2af7e8178ce03910f739
mphoChuene <mphochuene42@gmail.com>
2023-06-30 11:09:03 +0200

    admin page added

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,18 @@ import Hero from "./components/Hero/Hero.jsx";
 import Rooms from "./components/Rooms/Rooms.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import AdminPage from "./components/Admin/AdminPage.jsx";
+import Admin from "./components/Admin/Admin.jsx";
 import Category from "./components/category/Category.jsx";
 import Register from "./components/Register/Register.jsx";
 import Login from "./components/Login/Login.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/*   <Navbar />
+    {/*  <Navbar />
     <Hero />
     <Category />
     <Rooms />
     <Footer /> */}
-    <AdminPage />
+    <Admin />
   </React.StrictMode>
 );

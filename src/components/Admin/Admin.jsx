a52addff04a19591580b0934480316e345d3cfb7
mphoChuene <mphochuene42@gmail.com>
2023-07-01 10:09:04 +0200

    completed admin UI

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import styles from "./Admin.module.css";
+import {
+  basic,
+  comfort,
+  blueroom,
+  royal,
+  goldroom,
+  deluxroom,
+  superdelux,
+} from "../imports";
 
 //styling variables
 
@@ -33,6 +42,60 @@ const Admin = () => {
 
       <div className={styles.bar}>
         <h2>Hotel Suits</h2>
+      </div>
+      <div className={styles.unit_container}>
+        {/* //unit 1 */}
+        <div className={styles.unit}>
+          <img src={basic} alt="basic unit" />
+          <div className={styles.availability}>
+            <p>Mr Chuene</p> <br />
+            <p className={styles.sub_text}>
+              <span className={styles.icon_spacing}>
+                <i class="fa-solid fa-user"></i>X 2{" "}
+              </span>
+              <i class="fa-solid fa-moon"></i> 02 June-03 June
+            </p>
+          </div>
+        </div>
+        {/* //unit 2 */}
+        <div className={styles.unit}>
+          <img src={comfort} alt="comfort uint" />
+          <div className={styles.availability}>
+            <p>Mr Chuene</p> <br />
+            <p className={styles.sub_text}>
+              <span className={styles.icon_spacing}>
+                <i class="fa-solid fa-user"></i>X 2{" "}
+              </span>
+              <i class="fa-solid fa-moon"></i> 02 June-03 June
+            </p>
+          </div>
+        </div>
+        {/* //unit 3 */}
+        <div className={styles.unit}>
+          <img src={blueroom} alt="blueroom" />
+          <div className={styles.availability}>
+            <p>Mr Chuene</p> <br />
+            <p className={styles.sub_text}>
+              <span className={styles.icon_spacing}>
+                <i class="fa-solid fa-user"></i>X 2{" "}
+              </span>
+              <i class="fa-solid fa-moon"></i> 02 June-03 June
+            </p>
+          </div>
+        </div>
+        {/* //unit 4 */}
+        <div className={styles.unit}>
+          <img src={royal} alt="royal unit" />
+          <div className={styles.availability}>
+            <p>Mr Chuene</p> <br />
+            <p className={styles.sub_text}>
+              <span className={styles.icon_spacing}>
+                <i class="fa-solid fa-user"></i>X 2{" "}
+              </span>
+              <i class="fa-solid fa-moon"></i> 02 June-03 June
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero/Hero.jsx";
 import Rooms from "./components/Rooms/Rooms.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import AdminPage from "./components/Admin/AdminPage.jsx";
+import Admin from "./components/Admin/Admin.jsx";
 import Category from "./components/category/Category.jsx";
 import Register from "./components/Register/Register.jsx";
 import Login from "./components/Login/Login.jsx";
@@ -16,6 +16,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Category />
     <Rooms />
     <Footer /> */}
-    <AdminPage />
+    <Admin />
   </React.StrictMode>
 );

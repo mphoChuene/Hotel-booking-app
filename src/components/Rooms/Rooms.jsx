977e74f2f833bbd955a88c4910c20a995c40634d
mphoChuene <mphochuene42@gmail.com>
2023-10-04 16:55:25 +0200

    completed firebase UI

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,76 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Rooms.module.css";
-
-import {
-  basic,
-  comfort,
-  blueroom,
-  royal,
-  goldroom,
-  deluxroom,
-  superdelux,
-} from "../imports";
+import { useNavigate } from "react-router-dom";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../../firebase-config";
 
 const Rooms = () => {
+  const [units, setUnits] = useState([]);
+  const navigate = useNavigate();
+
+  const updateUnit = (id) => {
+    navigate(`/updateunit/${id}`);
+  };
+
+  const deleteUnit = async (id) => {
+    // Assuming 'bookings' is the correct collection name in your Firestore
+    // Replace 'bookings' with your actual collection name if needed
+    await deleteDoc(doc(db, "bookings", id));
+  };
+
+  const addRoom = () => {
+    navigate("/addunit");
+  };
+
+  const unitCollectionRef = collection(db, "bookings");
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(unitCollectionRef, (snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setUnits(data);
+    });
+
+    return () => unsubscribe();
+  }, [unitCollectionRef]);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>Available rooms</h1>
 
       <div className={styles.room_container}>
         <div className={styles.room_subcontainer}>
-          <div className={styles.slider_container}>
-            <div className={styles.box}>
-              <img src={basic} />
-              <h4>Twin room</h4>
-              <h5>R500 p/n</h5>
+          {/* Your existing room elements */}
+          {/* ... */}
 
-              <button className={styles.reserve}>Reserve</button>
-            </div>
-          </div>
-
-          <div className={styles.slider_container}>
-            <div className={styles.box}>
-              <img src={comfort} />
-              <h4>Basic-master</h4>
-              <h5>R1000 p/n</h5>
-
-              <button className={styles.reserve}>Reserve</button>
-            </div>
-          </div>
-          <div className={styles.slider_container}>
-            <div className={styles.box}>
-              <img src={blueroom} />
-              <h4>Luxury-master</h4>
-              <h5>R1250 p/n</h5>
-              <button className={styles.reserve}>Reserve</button>
-            </div>
-          </div>
-
-          <div className={styles.slider_container}>
-            <div className={styles.box}>
-              <img src={deluxroom} />
-              <h4>Royalty-suit</h4>
-              <h5>R2500 p/n</h5>
-              <button className={styles.reserve}>Reserve</button>
-            </div>
-          </div>
-          <div className={styles.slider_container}>
-            <div className={styles.box}>
-              <img src={comfort} />
-              <h4>Delux-comfort</h4>
-              <h5>R2800 p/n</h5>
-              <button className={styles.reserve}>Reserve</button>
-            </div>
-          </div>
-
-          <div className={styles.slider_container}>
-            <div className={styles.box}>
-              <img src={superdelux} />
-              <h4>Master-suit</h4>
-              <h5>R3000 p/n</h5>
-              <button className={styles.reserve}>Reserve</button>
-            </div>
-          </div>
+          {units.map((unit) => {
+            return (
+              <div className={styles.unit} key={unit.id}>
+                {/* Render your room data here */}
+                <img src={unit.Img} alt="royal unit" />
+                <div className={styles.availability}>
+                  <p>{unit.Guest}</p> <br />
+                  <p className={styles.sub_text}>
+                    <span className={styles.icon_spacing}>
+                      <i className="fa-solid fa-user"></i>X 2{" "}
+                    </span>
+                    <i className="fa-solid fa-moon"></i>
+                    {unit.Date}
+                  </p>
+                  <button className={styles.btn} onClick={() => updateUnit(unit.id)}>
+                    Update
+                  </button>
+                  <button className={styles.btn} onClick={() => deleteUnit(unit.id)}>
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
